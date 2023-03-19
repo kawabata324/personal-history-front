6f8f4937eb68e7b64edc552ae4475d5e8e3415e4
kawabata324 <kawabataharuki48@gmail.com>
2023-03-19 21:41:30 +0900

    contributor section add

---
+++ components/top/TheContributor.vue
@@ -0,0 +1,29 @@
+<script lang="ts" setup></script>
+
+<template>
+  <section class="pt-20 pb-48">
+    <h1 class="font-bold text-3xl text-center">コントリビューター</h1>
+    <div class="container mx-auto px-4 mt-10">
+      <div class="flex justify-center">
+        <div class="w-full md:w-6/12 lg:w-3/12 lg:mb-0 mb-12 px-4">
+          <div class="px-6">
+            <img
+                alt="kawabata"
+                src="usa.png"
+                class="shadow-lg rounded-full max-w-full mx-auto"
+                style="max-width: 120px;"
+            />
+            <div class="pt-6 text-center">
+              <h5 class="text-xl font-bold">Kawabata</h5>
+              <p class="mt-1 text-sm text-gray-500 uppercase font-semibold">
+                Web Developer
+              </p>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  </section>
+</template>
+
+<style scoped></style>
