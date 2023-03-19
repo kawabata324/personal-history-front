2d293ea664b45000fd90aafaaa813c378b8d21a7
kawabata324 <kawabataharuki48@gmail.com>
2023-03-19 16:43:18 +0900

    first init

---
+++ pages/index.vue
@@ -0,0 +1,9 @@
+<script lang="ts" setup></script>
+
+<template>
+  <div>
+    -- Top Page -- サービスの紹介などが入ります
+  </div>
+</template>
+
+<style scoped></style>
---
+++ pages/personalHistories/index.vue
@@ -0,0 +1,9 @@
+<script lang="ts" setup></script>
+
+<template>
+  <div>
+    -- 履歴書 一覧 --
+  </div>
+</template>
+
+<style scoped></style>
